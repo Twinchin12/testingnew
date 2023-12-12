811a4228f8436fd6941e5feb58c76ab91e457a78
zahid <zahidhasmani07@gmail.com>
2023-12-12 00:02:14 +0000

    add kernel config

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -116,7 +116,9 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_RAMDISK_USE_LZ4 := true
 TARGET_KERNEL_ADDITIONAL_FLAGS := BRAND_SHOW_FLAG=oneplus
 TARGET_KERNEL_SOURCE := kernel/oneplus/sm8250
-TARGET_KERNEL_CONFIG := evo_defconfig vendor/debugfs.config
+TARGET_KERNEL_CONFIG := vendor/kona-perf_defconfig vendor/debugfs.config
+KERNEL_LD := LD=ld.lld
+KERNEL_SUPPORTS_LLVM_TOOLS := true
 
 # Lineage Health
 TARGET_HEALTH_CHARGING_CONTROL_CHARGING_PATH := /sys/class/oplus_chg/battery/mmi_charging_enable
